64820b5bd0784ee7ef15927f272c170da3f4ea60
03-03-2001 <rohitvkamble0301@gmail.com>
2026-02-23 13:15:18 +0530

    Updated laundry services toggle and total calculation

--- index.js
+++ index.js
@@ -1,79 +1,106 @@
+
+
 emailjs
 
 
 
 
-let total = 0;
-const cart = document.getElementById('cart');
-const totalEl = document.getElementById('total');
+ let totalAmount = 0;
+
+  function addItem(serviceName, price) {
+    const serviceItem = event.target.closest(".service-item");
+
+    const addBtn = serviceItem.querySelector(".add");
+    const removeBtn = serviceItem.querySelector(".remove");
+
+    addBtn.style.display = "none";
+    removeBtn.style.display = "inline-block";
+
+    totalAmount += price;
+    document.getElementById("total").innerText = totalAmount;
+
+    console.log(serviceName + " added");
+  }
+
+  function removeItem(serviceName, price) {
+    const serviceItem = event.target.closest(".service-item");
+
+    const addBtn = serviceItem.querySelector(".add");
+    const removeBtn = serviceItem.querySelector(".remove");
+
+    removeBtn.style.display = "none";
+    addBtn.style.display = "inline-block";
+
+    totalAmount -= price;
+    document.getElementById("total").innerText = totalAmount;
+
+    console.log(serviceName + " removed");
+  }
 
 
-function addItem(name, price) {
- const item = document.createElement('div');
-  item.className = 'cart-item';
-  item.textContent = `${name} - ₹${price}`;
+
+
+
+const scrollToServices = () => {
+  document.getElementById("services").scrollIntoView({
+    behavior: "smooth"
+  });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  
-  cart.appendChild(item);
 
-
-total += price;
-totalEl.textContent = total;
-}
-
-
-function removeItem(name, price) {
-const items = document.querySelectorAll('.cart-item');
-for (let item of items) {
-if (item.textContent.includes(name)) {
-item.remove();
-total -= price;
-if (total < 0) total = 0;
-totalEl.textContent = total;
-break;
-}
-}
-}
-
-
-
-const url = "https://api.emaljs.com/api/v1.0/email/send"; 
-
-
-const data ={
-  name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    phone:document.getElementById("phone").value
-}
-  fetch("https://api.emailjs.com/api/v1.0/email/send", {
-  method: "POST",
-  body: JSON.stringify(data)
-});
-
-
-
-
-
-
-
-emailjs.init("WQp9xAbC12345");
-
-
- document.getElementById("contact-Form").addEventListener("click", function () {
-
-  emailjs.send("service_xxxx", "template_xxxx", {
+function sendMail (){
+  var params = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
-    phone:document.getElementById("phone").value
-  })
-  .then(() => {
-    alert("Thank you for booking! We will get back to you soon.");
+    phone:document.getElementById("phone").value,
+  }
+   const serviceID = "service_yrc4fq2"
+  const templateID = "template_9b137vc" 
+
+  emailjs.send(serviceID,templateID,params)
+
+  .then((response) => {
+     document.getElementById("name").value ="";
+     document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+     console.log('res');
+    alert("Thank you for booking! We will get back to you soon." ,response.status, response.text);
   })
   .catch(err => {
      console.error("EmailJS error:", err);
   });
 
-});
+
+
+}
+
+  
+
+
+
+
+
+
+ 
+
+
+
+
 
 
 
